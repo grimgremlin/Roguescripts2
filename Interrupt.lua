096d63ecc40491d901e90927d2e0c8f6cb76b9f7
grimgremlin <silentjay213@gmail.com>
2017-04-22 01:37:27 -0400

    Add files via upload

--- Interrupt.lua
+++ Interrupt.lua
@@ -21,7 +21,7 @@ function InterruptSay:COMBAT_LOG_EVENT_UNFILTERED(...)
 	local a = select(13, ...)
 	local spellid = select(12, ...)
 	
-	if aUser==UnitName("player") or aUser==UnitName("pet") or aUser=="Trickynick" or aTarget==UnitName("player") or aTarget=="Trickynick" then
+	if aUser==UnitName("player") or aUser==UnitName("pet") or aTarget==UnitName("player") then
 		if aEvent== 'SPELL_INTERRUPT' then
 			if inRaid then SendChatMessage(aTarget.."'s "..GetSpellLink(spellid).." was interrupted by "..aUser.."'s "..GetSpellLink(interruptid)..".","raid") end
 			if inParty and not inRaid then SendChatMessage(aTarget.."'s "..GetSpellLink(spellid).." was interrupted by "..aUser.."'s "..GetSpellLink(interruptid)..".","party") end
@@ -34,28 +34,11 @@ function InterruptSay:COMBAT_LOG_EVENT_UNFILTERED(...)
 				if inParty and not inRaid then SendChatMessage(aUser.." used "..GetSpellLink(interruptid).." on ["..aTarget.."].","party") end
 			end
 		end
-				
-			if aEvent== 'SPELL_AURA_REMOVED' then
-				--ChatFrame1:AddMessage(GetSpellLink(interruptid).." fades from "..aTarget..".")
-				if (c=="Soulstone Resurrection") then
-					if inRaid then SendChatMessage(GetSpellLink(interruptid).." has faded from "..aTarget..".","raid") end
-					if inParty and not inRaid then SendChatMessage(GetSpellLink(interruptid).." has faded from "..aTarget..".","party") end
-					if not inParty and not inRaid then SendChatMessage(GetSpellLink(interruptid).." has faded from "..aTarget..".","say") end
-				end
-				if (c=="Fel Armor") then ChatFrame1:AddMessage(GetSpellLink(interruptid).." has faded from "..aTarget..".") end
-				if (c=="Unending Breath") then ChatFrame1:AddMessage(GetSpellLink(interruptid).." has faded from "..aTarget..".") end
-				if (c=="Detect Invisibility") then ChatFrame1:AddMessage(GetSpellLink(interruptid).." has faded from "..aTarget..".") end
-				if (c=="Battle Shout") and aTarget=="Defensive" then ChatFrame1:AddMessage(GetSpellLink(interruptid).." has faded from "..aTarget..".") end
-			end
-			if aEvent== "SPELL_HEAL" then
-				if c=="Master Healthstone" then
-					if aTarget=="Trickynick" then 
-					if inRaid then SendChatMessage(aTarget.." has used a tasty "..GetSpellLink(interruptid)..".","raid") end
-					if inParty and not inRaid then SendChatMessage(aTarget.." has used a tasty "..GetSpellLink(interruptid)..".","party") end
-					if not inParty and not inRaid then SendChatMessage(aTarget.." has used a tasty "..GetSpellLink(interruptid)..".","say") end				
-				end
-			end
-			end
-	end		
+		if aEvent== 'SPELL_MISSED' and (c=="Mocking Blow" or c=="Taunt" or c=="Challenging Shout") then
+			if inRaid then SendChatMessage(GetSpellLink(interruptid).." was resisted by "..aTarget..".","raid") end
+			if inParty and not inRaid then SendChatMessage(GetSpellLink(interruptid).." was resisted by "..aTarget..".","party") end
+			if not inParty and not inRaid then SendChatMessage(GetSpellLink(interruptid).." was resisted by "..aTarget..".","say") end
+		end
+			end	
 end
 
